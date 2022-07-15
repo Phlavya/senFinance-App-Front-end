4face2bb8e24c73d1a94f9ecaf118d182c710cad
Flávia Braga <Bragarflavia@gmail.com>
2022-07-15 05:47:16 -0300

    Create Index.js

---
+++ SRC/Formcomponentes/Campo de texto/Index.js
@@ -0,0 +1,22 @@
+import React from "react"; 
+ import { TextField } from "@mui/material"; 
+ import { useField } from "formik"; 
+  
+ function TextFieldWrapper({ name, ...otherProps }) { 
+   const [field, data] = useField(name); 
+  
+   const configTextfield = { 
+     ...field, 
+     ...otherProps, 
+     fullWidth: true, 
+   }; 
+  
+   if (data && data.touched && data.error) { 
+     configTextfield.error = true; 
+     configTextfield.helperText = data.error; 
+   } 
+  
+   return <TextField {...configTextfield} />; 
+ } 
+  
+ export default TextFieldWrapper;
