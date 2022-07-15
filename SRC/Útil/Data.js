c0b23a17c3807cb52bc115c9a5b02927e48a71e3
Flávia Braga <Bragarflavia@gmail.com>
2022-07-15 04:50:17 -0300

    Create Data.js

---
+++ SRC/Útil/Data.js
@@ -0,0 +1,26 @@
+export function dataAtualFormatada() { 
+   var data = new Date(); 
+   var dia = data.getDate().toString(); 
+   var diaF = dia.length === 1 ? "0" + dia : dia; 
+   var mes = (data.getMonth() + 1).toString(); 
+   var mesF = mes.length === 1 ? "0" + mes : mes; 
+   var anoF = data.getFullYear(); 
+   var hora = data.getHours() < 10 ? "0" + data.getHours() : data.getHours(); 
+   var minuto = 
+     data.getMinutes() < 10 ? "0" + data.getMinutes() : data.getMinutes(); 
+   var segundos = 
+     data.getSeconds() < 10 ? "0" + data.getSeconds() : data.getSeconds(); 
+   return ( 
+     diaF + 
+     "/" + 
+     mesF + 
+     "/" + 
+     anoF + 
+     " às " + 
+     hora + 
+     ":" + 
+     minuto + 
+     ":" + 
+     segundos 
+   ); 
+ }
